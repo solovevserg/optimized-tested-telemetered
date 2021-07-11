10e6761830624434994ac468495baad218c0f4b4
Sergei <sergey.soloviev@inbox.ru>
2021-07-11 20:11:17 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qoollo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'optimized-tested-telemetered';
+}
